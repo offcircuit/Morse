2d450b3ab3672340aa30a5009724569f0d70f0ca
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-13 22:15:07 +0000

    Add files via upload

---
+++ examples/Morse.ino
@@ -0,0 +1,36 @@
+#include "Morse.h"
+
+Morse morse;
+
+String s;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  morse.event = callback;
+  morse.transmit("sending morse code...");
+  Serial.println(s);
+  Serial.println(morse.receipt(s));
+}
+
+void callback(int8_t *e) {
+  s += String(*e);
+
+  if (*e == MORSE_GAP) Serial.print(" GAP ");
+  if (*e == MORSE_CHAR) Serial.println(" CHAR ");
+  else if (*e == MORSE_WORD) Serial.println(" WORD ");
+  else if (*e == MORSE_PHRASE) Serial.println(" PHRASE ");
+  else {
+    if (*e == MORSE_DI) Serial.print(" DI ");
+    else if (*e == MORSE_DIT) Serial.print(" DIT ");
+    else if (*e == MORSE_DAH) Serial.print(" DAH ");
+    else {
+      Serial.print(" ");
+    }
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
