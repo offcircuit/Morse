788040e8fd4ca5fc62b249d299c2cb776f3c5fdb
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-14 17:44:29 +0000

    Update serial.ino

--- examples/serial/serial.ino
+++ examples/serial/serial.ino
@@ -7,30 +7,48 @@ String s;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  morse.event = callback;
-  morse.transmit("sending morse code...");
+
+  Serial.println("SEND MESSAGE ----------------------------------------------------------");
+
+  morse.transmit = callback;
+  morse.write("send<ing morse code ...<");
+
+  Serial.println("");
+  Serial.println("PRINT CODE ------------------------------------------------------------");
   Serial.println(s);
-  Serial.println(morse.receipt(s));
-}
 
-void callback(int8_t *e) {
-  s += String(*e);
+  Serial.println("");
+  Serial.println("CONVERT CODE ----------------------------------------------------------");
+  Serial.println(morse.read(s));
 
-  if (*e == MORSE_GAP) Serial.print(" GAP ");
-  if (*e == MORSE_CHAR) Serial.println(" CHAR ");
-  else if (*e == MORSE_WORD) Serial.println(" WORD ");
-  else if (*e == MORSE_PHRASE) Serial.println(" PHRASE ");
-  else {
-    if (*e == MORSE_DI) Serial.print(" DI ");
-    else if (*e == MORSE_DIT) Serial.print(" DIT ");
-    else if (*e == MORSE_DAH) Serial.print(" DAH ");
-    else {
-      Serial.print(" ");
-    }
+  Serial.println("");
+  Serial.println("CONVERT CODE STEP BY STEP ---------------------------------------------");
+  for (int i = 0; i < s.length(); i++) {
+    Serial.print(morse.receive(String(s[i]).toInt()));
+    delay(20);
   }
+
+  Serial.println("");
+  Serial.println("-----------------------------------------------------------------------");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void callback(int8_t e) {
+  s += String(e);
+  if (e == MORSE_GAP) Serial.print(" GAP ");
+  if (e == MORSE_CHAR) Serial.println(" CHAR ");
+  else if (e == MORSE_WORD) Serial.println(" WORD ");
+  else if (e == MORSE_PHRASE) Serial.println(" PHRASE ");
+  else {
+    if (e == MORSE_DI) Serial.print(" DI ");
+    else if (e == MORSE_DIT) Serial.print(" DIT ");
+    else if (e == MORSE_DAH) Serial.print(" DAH ");
+    else {
+      Serial.print(" ");
+    }
+  }
 }
