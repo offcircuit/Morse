6165c0e0d676291f16303d863a4305394e96e86e
Helder Luciano da Costa Rodrigues <35690030+offcircuit@users.noreply.github.com>
2019-03-15 13:30:37 +0000

    Update serial.ino

--- examples/serial/serial.ino
+++ examples/serial/serial.ino
@@ -4,41 +4,14 @@ Morse morse;
 
 String s;
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-
-  Serial.println("SEND MESSAGE ----------------------------------------------------------");
-
-  morse.transmit = callback;
-  morse.write("send<ing morse code ...<");
-
-  Serial.println("");
-  Serial.println("PRINT CODE ------------------------------------------------------------");
-  Serial.println(s);
-
-  Serial.println("");
-  Serial.println("CONVERT CODE ----------------------------------------------------------");
-  Serial.println(morse.read(s));
-
-  Serial.println("");
-  Serial.println("CONVERT CODE STEP BY STEP ---------------------------------------------");
-  for (int i = 0; i < s.length(); i++) {
-    Serial.print(morse.receive(String(s[i]).toInt()));
-    delay(20);
-  }
-
-  Serial.println("");
-  Serial.println("-----------------------------------------------------------------------");
+void receiver(char e) {
+  Serial.print(e);
+  delay(50);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
-}
-
-void callback(int8_t e) {
+void transmiter(int8_t e) {
   s += String(e);
+
   if (e == MORSE_GAP) Serial.print(" GAP ");
   if (e == MORSE_CHAR) Serial.println(" CHAR ");
   else if (e == MORSE_WORD) Serial.println(" WORD ");
@@ -51,4 +24,31 @@ void callback(int8_t e) {
       Serial.print(" ");
     }
   }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  morse.begin(transmiter, receiver);
+  
+  Serial.println("-- WRITE MESSAGE --");
+  morse.write("sending ");
+  morse.writeln("morse code...");
+  Serial.println("");
+
+  Serial.println("-- MESSAGE RECEIVED --");
+  Serial.println(s);
+  Serial.println("");
+  
+  Serial.println("-- READ RECEIVED --");
+  Serial.println(morse.read(s));
+  Serial.println("");
+  
+  Serial.println("-- READ RECEIVED STEP BY STEP --");
+  for (int i = 0; i < s.length(); i++) morse.listen(String(s[i]).toInt());
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
 }
